a0a0c786951bebd3e0fee2be367de83e8f022812
BrendonPierson <brendonpierson@gmail.com>
2015-10-23 14:31:06 -0500

    Completed NaturalNumbers implementation with tests.

---
+++ Mathmagician/NaturalNumbers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class NaturalNumbers
+    {
+        // Anything outside of the NaturalNumbers class can't access first
+        // Known as a data member (don't put literals in methods)
+        private int first;
+
+        // Constructor method explicitly declared, overriding default
+        // Runs everytime we make a new instance
+        public NaturalNumbers()
+        {
+            first = 1;
+        }
+
+        // mapping expression works for any number after 1
+        public int GetNext(int x)
+        {
+            return x + 1;
+        }
+
+        // Public Method that returns int, inside NaturalNumbers class
+        // Base case
+        public int GetFirst()
+        {
+            return first;
+        }
+
+        public int[] GetSequence()
+        {
+            return GetSequence(10);
+        }
+
+        // overloaded methods, an ability of object oriented programming
+        public int[] GetSequence(int length)
+        {
+            List<int> output = new List<int>();
+            output.Add(GetFirst());
+            while (output.Count < length)
+            {
+                output.Add(GetNext(output.Last()));
+            }
+            return output.ToArray();
+        }
+
+        public string ToString(int[] array)
+        {
+            string output = String.Join(" ", array);
+            return output;
+        }
+    }
+}
---
+++ MathmagicianTests/NaturalNumbersTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// Need to add using namespace of the other project
+using Mathmagician;
+
+namespace MathmagicianTests
+{
+    [TestClass]
+    public class NaturalNumbersTests
+    {
+        [TestMethod]
+        public void EnsureICanCreateClassInstance()
+        {
+            // Bootstrapping test because you have yet to create a class
+            // Make sure the build workflow is consistent and works well
+            NaturalNumbers nats = new NaturalNumbers();
+            // When you write tests you can ignore red squiggly lines because the class doesn't exist yet
+            
+            // Every test has the form of setup then assert
+            Assert.IsNotNull(nats);
+        }
+
+        [TestMethod]
+        public void ShouldPrintFirstNumber()
+        {
+            NaturalNumbers nats = new NaturalNumbers();
+            int expected = 1;
+            int actual = nats.GetFirst();
+            Assert.AreEqual(expected, actual);
+        }
+
+        /*
+        Educational Purposes example exception test
+
+        [TestMethod]
+        [ExpectedException(typeof(AccessViolationException))]
+        // Exceptions themselves are just classes
+        public void NoOneCanAccessPrivateDataMember()
+        {
+            NaturalNumbers nats = new NaturalNumbers();
+            nats.first;
+        }
+        */
+
+        [TestMethod]
+        public void ShouldPrintNextNumber()
+        {
+            NaturalNumbers nats = new NaturalNumbers();
+            int input = 1;
+            int expected = 2;
+            int actual = nats.GetNext(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldPrintNextNumberInput3()
+        {
+            NaturalNumbers nats = new NaturalNumbers();
+            int input = 3;
+            int expected = 4;
+            int actual = nats.GetNext(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldPrintSequenceOfNumbers()
+        {
+            NaturalNumbers nats = new NaturalNumbers();
+            int[] expected = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] actual = nats.GetSequence();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldPrintAVariableSequenceOfNumbers()
+        {
+            NaturalNumbers nats = new NaturalNumbers();
+            int input = 8;
+            int[] expected = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int[] actual = nats.GetSequence(input);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldProvideSameSequences()
+        {
+            NaturalNumbers nats = new NaturalNumbers();
+            int input = 10;
+            int[] sequencewithlength = nats.GetSequence(input);
+            int[] sequencenolength = nats.GetSequence();
+            CollectionAssert.AreEqual(sequencenolength, sequencewithlength);
+        }
+
+        [TestMethod]
+        public void PrintSequenceOfNumbers()
+        {
+            NaturalNumbers nats = new NaturalNumbers();
+            int length = 7;
+            string expected = "1 2 3 4 5 6 7";
+            string actual = nats.ToString(nats.GetSequence(length));
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
