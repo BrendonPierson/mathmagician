7ca56bf190cc2f120e89e4e6fe97ab2d3d7fa997
BrendonPierson <brendonpierson@gmail.com>
2015-10-23 15:48:11 -0500

    Commented out getnext override in favor of protected variable

--- Mathmagician/EvenNumbers.cs
+++ Mathmagician/EvenNumbers.cs
@@ -11,15 +11,16 @@ namespace Mathmagician
         public EvenNumbers()
         {
             first = 2;
+            increment = 2;
         }
 
-        // Override changes a previously defined method
-        public override int GetNext(int x)
-        {
-            // return x + 2;
-            // call parent method then add 1 to returned value
-            return base.GetNext(x) + 1;
-        }
+        //// Override changes a previously defined method
+        //public override int GetNext(int x)
+        //{
+        //    // return x + 2;
+        //    // call parent method then add 1 to returned value
+        //    return base.GetNext(x) + 1;
+        //}
 
 
     }
