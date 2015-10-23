6db429b35fae74f659b2d63e11a7918c583dd17c
BrendonPierson <brendonpierson@gmail.com>
2015-10-23 17:09:49 -0500

    Added FibonacciNumber class and tests

---
+++ Mathmagician/FibonacciNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class FibonacciNumbers : NaturalNumbers
+    {
+        public FibonacciNumbers()
+        {
+            first = 1;
+            increment = 2;
+        }
+
+        public int GetNext(int x, int y)
+        {
+            // return x + 2;
+            // call parent method then add 1 to returned value
+            return x + y;
+        }
+
+        new public int[] GetSequence(int length)
+        {
+            List<int> output = new List<int>();
+            output.Add(GetFirst());
+            output.Add(GetFirst());
+            while (output.Count < length)
+            {
+                output.Add(GetNext(output.Last(), output[output.Count() - 2]));
+                
+            }
+            return output.ToArray();
+        }
+    }
+}
---
+++ MathmagicianTests/FibonacciTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mathmagician;
+
+namespace MathmagicianTests
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        public void FibonacciEnsureICanCreateClassInstance()
+        {
+            FibonacciNumbers fib = new FibonacciNumbers();
+            Assert.IsNotNull(fib);
+        }
+
+        [TestMethod]
+        public void FibonacciShouldPrintFirstNumber()
+        {
+            FibonacciNumbers fib = new FibonacciNumbers();
+            int expected = 1;
+            int actual = fib.GetFirst();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibonacciShouldPrintNextNumber()
+        {
+            FibonacciNumbers fib = new FibonacciNumbers();
+            int inputA = 2;
+            int inputB = 3;
+            int expected = 5;
+            int actual = fib.GetNext(inputA, inputB);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibonacciShouldPrintAVariableSequenceOfNumbers()
+        {
+            FibonacciNumbers fib = new FibonacciNumbers();
+            int input = 8;
+            int[] expected = new int[] { 1,1,2,3,5,8,13,21 };
+            int[] actual = fib.GetSequence(input);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibonacciPrintSequenceOfNumbers()
+        {
+            FibonacciNumbers fib = new FibonacciNumbers();
+            int length = 7;
+            string expected = "1 1 2 3 5 8 13";
+            string actual = fib.ToString(fib.GetSequence(length));
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
